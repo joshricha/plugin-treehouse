9a34801b088a751ae53525b4b2821a11caff0d28
Josh Richardson <joshrichardson1234@hotmail.com>
2015-08-04 11:33:50 +1000

    Start Treehouse plugin

---
+++ wp-content/plugins/wptreehouse-badges/wptreehouse-badges.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*
+ *	Plugin Name: Treehouse Badges Plugin
+ *	Plugin URI: http://wptreehouse.com/wptreehouse-badges-plugin/
+ *	Description: Provides both widgets and shortcodes to help you display your Treehouse profile badges on your website.  The official Treehouse badges plugin.
+ *	Version: 1.0
+ *	Author: Josh Richardson
+ *	Author URI: http://joshuarichardson.com.au
+ *	License: GPL2
+ *
+*/
+
+
+
+
+?>
